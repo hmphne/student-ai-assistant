a477b385715d18333e6768a71daf1390b11b5fd4
hmphne <3rdoperson@gmail.com>
2025-04-04 13:21:24 +0330

    feat: added theme

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { THEME } from "@/utils/constants";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme={THEME}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
